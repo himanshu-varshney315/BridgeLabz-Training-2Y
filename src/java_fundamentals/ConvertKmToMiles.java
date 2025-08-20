5cfd9bfa5f99fa7d5c973e20d37c7d87aeae15e7
himanshu-varshney315 <hg4770114@gmail.com>
2025-08-20 16:03:10 +0530

    Java core

---
+++ src/java_fundamentals/ConvertKmToMiles.java
@@ -0,0 +1,12 @@
+package java_fundamentals;
+
+import java.util.Scanner;
+
+public class ConvertKmToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a= 10.8;
+        System.out.printf("The distance 10.8 km in miles is %.2f",a*1.6);
+
+    }
+}
---
+++ src/java_fundamentals/SamAverageMarks.java
@@ -0,0 +1,13 @@
+package java_fundamentals;
+
+import java.util.Scanner;
+
+public class SamAverageMarks {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int a= 94;
+        int b= 95;
+        int c= 96;
+        System.out.printf("Sam’s average mark in PCM is %d",(a+b+c)/3);
+    }
+}
