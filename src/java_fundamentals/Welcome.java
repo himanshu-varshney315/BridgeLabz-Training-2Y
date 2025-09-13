880ca4e2b8e710eb5acc77abeef965d7b6e54bc3
Himanshu Gupta <hg4770114@gmail.com>
2025-09-13 20:08:00 +0530

    Delete src directory

--- src/java_fundamentals/Welcome.java
+++
@@ -1,9 +0,0 @@
-package java_fundamentals;
-
-public class Welcome {
-    public static void main(String[] args) {
-        System.out.print("HELLO WORLD\n");
-        System.out.print("Welcome to Bridgelabz!");
-    }
-}
-
